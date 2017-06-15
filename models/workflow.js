c142fe59e96c2b0b3e2176c46963ca2d542385fc
bintjes <bintjes@gmail.com>
2017-06-15 16:13:47 +0200

    Adde dindex.js

--- models/workflow.js
+++ models/workflow.js
@@ -1,7 +1,7 @@
 /* jshint indent: 2 */
 
 module.exports = function(sequelize, DataTypes) {
-  return sequelize.define('workflow', {
+  return sequelize.define('Workflow', {
     id: {
       type: DataTypes.INTEGER(10).UNSIGNED,
       allowNull: false,
